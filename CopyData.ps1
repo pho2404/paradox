6f6275dff410eea3f4ded9b1917161e86ec16ca7
pho2404 <59258650+pho2404@users.noreply.github.com>
2020-04-13 11:44:05 +0200

    Create Structure and copy data

---
+++ CopyData.ps1
@@ -0,0 +1,47 @@
+﻿
+Add-Type -Path "C:\Program Files (x86)\System.Data.SQLite\2010\bin\System.Data.SQLite.dll"
+
+function CopyTable {
+Param ([string]$from, [string]$to,[string]$dsn)
+
+
+$conSource = New-Object System.Data.Odbc.OdbcConnection
+$conSource.ConnectionString ="DSN=$dsn"
+$conSource.Open()
+Write-Output ("Connection open")
+$cmdSource = New-Object System.Data.Odbc.OdbcCommand("select * from $from" ,$conSource)
+Write-Output ("Reading")
+$dr = $cmdSource.ExecuteReader()
+Write-Output ("Loading data")
+$dt = New-Object System.Data.DataTable
+$dt.Load($dr)
+
+Write-Output ("Creating Destination")
+$conDest = New-Object System.Data.SQLite.SQLiteConnection
+$conDest.ConnectionString = "Data Source=c:\tools\sqllite\$dsn.db"
+
+$da = New-Object System.Data.SQLite.SQLiteDataAdapter("select * from $to", $conDest)
+$cb = New-Object System.Data.SQLite.SQLiteCommandBuilder($da)
+$cb.QuotePrefix = "[";  
+$cb.QuoteSuffix = "]";  
+
+Write-Output ("Copied table")
+
+
+$dtDest = New-Object System.Data.DataTable
+$dtDest = $dt.Copy()
+
+Write-Output ("Changing Status")
+for ($i=0;$dtDest.Rows.Count-$i;$i++)
+{
+    $dtDest.Rows[$i].SetAdded();
+}
+
+$da.UpdateBatchSize = 0
+
+Write-Output ("Inserting data")
+$da.Update($dtDest)
+Write-Output ("Done.")
+}
+
+CopyTable -from "AWPatient" -to "AWPatient" -dsn "IKR"
---
+++ CreateStructure.ps1
@@ -0,0 +1,65 @@
+﻿
+function CreateTableScript {
+Param ([string]$name)
+
+$con = New-Object System.Data.Odbc.OdbcConnection
+$con.ConnectionString ="DSN=GAP"
+
+
+$cmd = New-Object System.Data.Odbc.OdbcCommand("select * from $name where 1 = 0" ,$con)
+$cmd.Connection = $con
+
+$con.Open()
+$dr = $cmd.ExecuteReader()
+$schema = $dr.GetSchemaTable();
+
+$e = New-Object System.Data.SqlDbType
+
+
+if(!($schema.Rows.Count -eq 0))
+{
+    Write-Output "Create table $name ("
+    
+    for($i = 0; $schema.Rows.Count-$i;$i++) 
+    { 
+    if($schema.Rows[$i].DataType.Name -eq "DateTime")
+    {
+        $e.value__ = 4
+    }
+    else
+    { 
+        $e.value__ = $schema.Rows[$i].ProviderType
+    }
+     
+        $line = "[" + $schema.Rows[$i].ColumnName + "] " + $e.ToString()
+        if($e.value__ -eq 3 -or $e.value__ -eq 10  -or $e.value__ -eq 11  -or $e.value__ -eq 11  -or $e.value__ -eq 12  -or $e.value__ -eq 22 )
+        {
+            $line = $line + "(" + [string]$schema.Rows[$i].ColumnSize + ")"
+        }
+        if(!($schema.Rows.Count-$i -eq 1))
+        {
+            $line = $line + ","
+        }
+        Write-Output ($line)
+    }
+
+    Write-Output (");")
+    Write-Output ("")
+}
+else
+{
+    Write-Output ("")
+     Write-Output ("-- Unable to string table $name")
+     Write-Output ("")
+}
+
+$con.Close()
+}
+
+
+CreateTableScript "AWAgenda"
+CreateTableScript "AWPatient"
+
+#Get-ChildItem -Path C:\GAP\GapTS\Dossier\IKR\*.db | ForEach-Object {
+#    CreateTableScript $_.BaseName
+#}
